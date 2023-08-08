bac9c742298c043237da2b8da66aff0835dfdca9
root <root@titan154.lab.eng.tlv2.redhat.com>
2023-08-08 15:23:35 +0300

    Added api verifications

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,29 +1,111 @@
 import openai
+import time
 
 INPUT = ["Dogs don't like cats", 
          "Cats don't like dogs", 
          "Humans like dogs and cats"]
+
+CHAT_MESSAGES=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "My name is Sergey! What is your name?"},
+  ]
+
+EMBEDDING_MODELS = ['sentence-transformers/all-MiniLM-L6-v2', 
+                    'sentence-transformers/all-MiniLM-L12-v2']
+COMPLETION_MODELS = ['/models/vicuna-7b/']
+
+# Test list all models method
+def model_list():
+    models = openai.Model.list()
+    print(models)
+
+# Test embeddings method
+def embeddings(model_name, input):
+    resp = openai.Embedding.create(
+        input=input,
+        model=model_name)
+    emb_len = len(resp['data'][0]['embedding'])
+    res = {
+        "model_name": model_name,
+        "embeddings_size": emb_len
+        }
+    print(res)
+
+# Test Complition method
+def complition(model_name, input, n=1, stream=False):
+    if not stream:
+        completion = openai.Completion.create(
+        model=model_name,
+        n=n,
+        prompt=input,
+        )
+        print(completion)
+    else:
+        start_time = time.time()
+        completion = openai.Completion.create(
+        model=model_name,
+        n=n,
+        prompt=input,
+        stream=True
+        )
+        # create variables to collect the stream of chunks
+        collected_chunks = []
+        collected_messages = []
+        # iterate through the stream of events
+        for chunk in completion:
+            chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+            collected_chunks.append(chunk)  # save the event response
+            chunk_message = chunk['choices'][0]['text']  # extract the message
+            collected_messages.append(chunk_message)  # save the message
+            print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")  # print the delay and text
+
+# Test ChatComplition method
+def chat_complition(model_name, input, n=1, stream=False):
+    if not stream:
+        completion = openai.ChatCompletion.create(
+        model=model_name,
+        n=n,
+        messages=input,
+        )
+        print(completion)
+    else:
+        start_time = time.time()
+        completion = openai.ChatCompletion.create(
+        model=model_name,
+        n=n,
+        messages=input,
+        stream=True
+        )
+        # create variables to collect the stream of chunks
+        collected_chunks = []
+        collected_messages = []
+        # iterate through the stream of events
+        for chunk in completion:
+            chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+            collected_chunks.append(chunk)  # save the event response
+            chunk_message = chunk['choices'][0]['delta']  # extract the message
+            collected_messages.append(chunk_message)  # save the message
+            print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message}")  # print the delay and text
+
+        # print the time delay and text received
+        print(f"Full response received {chunk_time:.2f} seconds after request")
+        full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+        print(f"Full conversation received: {full_reply_content}")
+
 
 if __name__ == '__main__':
     openai.api_base = 'http://localhost:7000/v1'
     openai.organization = ''
     openai.api_key = ''
     
-    # Test list models method
-    models = openai.Model.list()
-    print(models)
-
-    # Test embeddings method
-    result_list = []
-    for model_spec in models['data'][1:]:
-        resp = openai.Embedding.create(
-        input=INPUT,
-        model=model_spec['id'])
-        emb_len = [len(emb['embedding']) for emb in resp['data']]
-        result_list.append(
-            {
-                "model_name": model_spec['id'],
-                "embeddings": emb_len
-            }
-        )
-    print(result_list)
+    model_list()
+    
+    for model in EMBEDDING_MODELS:
+        embeddings(model, INPUT)
+    
+    for model in COMPLETION_MODELS:
+        complition(model, INPUT[0])
+        complition(model, INPUT)
+        complition(model, INPUT, stream=True)
+        chat_complition(model, CHAT_MESSAGES)
+        chat_complition(model, CHAT_MESSAGES, stream=True)
