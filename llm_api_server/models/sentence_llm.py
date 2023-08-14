b8c75a798202c171378bd96edc82a25b111a1069
root <root@titan154.lab.eng.tlv2.redhat.com>
2023-08-14 17:14:47 +0300

    Fix device mapping

--- llm_api_server/models/sentence_llm.py
+++ llm_api_server/models/sentence_llm.py
@@ -9,13 +9,20 @@ class SentenceLLM(EmbeddingsLLM):
     def __init__(self, model_name: str, **kwargs) -> None:
         self.model_name = model_name
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        device = kwargs.get('device', None)
-        if 'instructor' in self.model_name:
-            self.model = INSTRUCTOR(self.model_name, device=device)
+        device = kwargs.get('device_map', None)
+        if device is None:
+            if torch.cuda.is_available():
+                self.device = torch.device('cuda:0')
+            else:
+                self.device = torch.device('cpu')
         else:
-            self.model = sentence_transformers.SentenceTransformer(self.model_name,
-                                                                    device=device)
-        self.device = self.model.device
+            self.device = torch.device(device)
+
+        if 'instructor' in self.model_name:
+            self.model = INSTRUCTOR(self.model_name)
+        else:
+            self.model = sentence_transformers.SentenceTransformer(self.model_name)
+        self.model = self.model.to(self.device)
 
     def get_token_count(self, prompt: str) -> int:
         count = self.tokenizer(prompt, return_length=True, return_tensors='np')
