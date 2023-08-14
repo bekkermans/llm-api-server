08a659537245075fb3d9bb6ff31377c8127d6e21
root <root@titan154.lab.eng.tlv2.redhat.com>
2023-08-14 16:10:40 +0300

    Added InstructorEmbedding & Refactor

--- llm_api_server/models/base.py
+++ llm_api_server/models/base.py
@@ -1,22 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Generator
 from api_spec import ChatCompletionsRequest
-import torch.cuda
-from transformers import AutoTokenizer
 
 
 class EmbeddingsLLM(ABC):
-    def __init__(self, model_name: str) -> None:
-        self.model_name = model_name
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        if torch.cuda.is_available():
-            self.device = torch.device('cuda:0')
-        else:
-            self.device = torch.device('cpu')
-
+    @abstractmethod
     def get_token_count(self, prompt: str) -> int:
-        count = self.tokenizer(prompt, return_length=True, return_tensors='np')
-        return int(count['length'][0])
+        pass
 
     @abstractmethod
     def encode(self, prompt: list) -> list:
--- llm_api_server/models/sentence_llm.py
+++ llm_api_server/models/sentence_llm.py
@@ -1,19 +1,26 @@
 import torch
-try:
-    import sentence_transformers
-except ImportError as e:
-    raise ImportError("Dependencies for sentence_transformers not found.")
-
+import sentence_transformers
+from InstructorEmbedding import INSTRUCTOR
+from transformers import AutoTokenizer
 from models.base import EmbeddingsLLM
 
 
 class SentenceLLM(EmbeddingsLLM):
-    def __init__(self, model_name: str) -> None:
-        super().__init__(model_name)
-        self.model = sentence_transformers.SentenceTransformer(model_name)
-        self.model = self.model.to(self.device)
+    def __init__(self, model_name: str, **kwargs) -> None:
+        self.model_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        device = kwargs.get('device', None)
+        if 'instructor' in self.model_name:
+            self.model = INSTRUCTOR(self.model_name, device=device)
+        else:
+            self.model = sentence_transformers.SentenceTransformer(self.model_name,
+                                                                    device=device)
+        self.device = self.model.device
+
+    def get_token_count(self, prompt: str) -> int:
+        count = self.tokenizer(prompt, return_length=True, return_tensors='np')
+        return int(count['length'][0])
 
     @torch.inference_mode()
     async def encode(self, prompt: list) -> list:
         return self.model.encode(prompt).tolist()
-
